d678a2091b6303a4a2fd86635152ac9f3361635d
vladskakun <52419346+vladskakun@users.noreply.github.com>
2021-11-18 10:39:56 +0200

    Modify Helper

--- EventExpress/Helper.cs
+++ EventExpress/Helper.cs
@@ -4,7 +4,14 @@ namespace EventExpress
 {
     public class Helper
     {
-        public static string GetProjectDirectory()
+        public static string GetProjectBinDirectory()
             => Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        public static string GetProjectDirectory()
+        {
+            string df = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            return $"{df.Substring(0, df.IndexOf("\\bin\\Debug"))}";
+        }
+
     }
 }
