d65094958203597c149904eae52cf01a16a64877
vladskakun <52419346+vladskakun@users.noreply.github.com>
2021-11-18 10:09:01 +0200

    Fix string

--- EventExpress/Helper.cs
+++ EventExpress/Helper.cs
@@ -5,9 +5,6 @@ namespace EventExpress
     public class Helper
     {
         public static string GetProjectDirectory()
-        {
-            string df = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            return $"{df.Substring(0, df.IndexOf("\\bin\\Debug"))}";
-        }
+            => Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
     }
 }
