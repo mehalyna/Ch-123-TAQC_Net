99ab799e223f23d65928796bcdf961a0618381ea
olyastrilectka <ospewag@gmail.com>
2023-07-20 13:53:48 +0200

    Delete comment

--- EventExpress/Tests/UserData.cs
+++ EventExpress/Tests/UserData.cs
@@ -2,7 +2,7 @@
 {
     public static class UserData
     {
-        public static string EmailAdmin = "AlexTestQC@gmail.com";
+        public static string EmailAdmin = "AlexTestQC@gmail.comgit";
         public static string PasswordAdmin = "123456";
         public static string BaseUrl = "https://eventsexpress-test.azurewebsites.net/";
     }
